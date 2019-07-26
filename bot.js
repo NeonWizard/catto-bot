f53f00c6e5aa4a3176f08fc2e467a89e2c1cc8cc
Spooky <averagewizard13@gmail.com>
2019-07-25 18:49:43 -0600

    Begin basic bot structure

--- bot.js
+++ bot.js
@@ -1,29 +1,54 @@
-const Discord = require('discord.js');
-const auth = require('./auth.json');
+const {Client} = require("discord.js");
+const fs = require("fs");
+const path = require("path");
 
-// Initialize Discord Bot
-const client = new Discord.Client();
+// -- Helper funcs
+const print = console.log;
+global.print = print;
 
-client.on("ready", () => {
-	console.log(`Logged in as ${client.user.tag}!`);
-});
+const BotEnv = class {
+	constructor (config, client) {
+		print("Client initialized.");
 
-client.on("message", msg => {
-	// Our bot needs to know if it will execute a command
-	// It will listen for messages that will start with `!`
-	if (msg.content.substring(0, 1) == "!") {
-		let args = msg.content.substring(1).split(" ");
-		let cmd = args[0];
+		this.config = config;
+		this.client = client;
 
-		args = args.splice(1);
-		switch(cmd) {
-			// !ping
-			case "ping":
-				msg.channel.send("pong");
-				break;
-			// Just add any case commands if you want to..
-		 }
-	 }
-});
+		this.prepare().then(this.start.bind(this)).catch(console.error);
+	}
 
-client.login(auth.token);
+	async prepare() {
+
+	}
+
+	async start() {
+		await this.client.login(this.config.token);
+
+		print("LOGIN COMPLETE");
+	}
+}
+
+new BotEnv(require("./config.js"), new Client({
+	disableEveryone: true
+}));
+
+// client.on("ready", () => {
+// 	console.log(`Logged in as ${client.user.tag}!`);
+// });
+
+// client.on("message", msg => {
+// 	// Our bot needs to know if it will execute a command
+// 	// It will listen for messages that will start with `!`
+// 	if (msg.content.substring(0, 1) == "!") {
+// 		let args = msg.content.substring(1).split(" ");
+// 		let cmd = args[0];
+
+// 		args = args.splice(1);
+// 		switch(cmd) {
+// 			// !ping
+// 			case "ping":
+// 				msg.channel.send("pong");
+// 				break;
+// 			// Just add any case commands if you want to..
+// 		 }
+// 	 }
+// });
