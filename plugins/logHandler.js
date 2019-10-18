3dfb6329b2eb8f44484e155c4757d2b87d7b1dd5
Spooky <averagewizard13@gmail.com>
2019-10-18 03:29:29 -0600

    Add message deletion logging

--- plugins/logHandler.js
+++ plugins/logHandler.js
@@ -37,5 +37,37 @@ module.exports = async function() {
         await channels.staff_logs.send({embed});
     };
 
+    const deleteEvent = async (message) => {
+        const author = message.author;
+        const channel = message.channel;
+        if (author.id == client.user.id) return;
+
+        const embed = {
+            color: 0xff3539,
+            author: {
+                name: author.tag,
+                icon_url: author.avatarURL
+            },
+            description: `**${author.tag}** deleted their message.`,
+            fields: [
+                {
+                    name: "Channel",
+                    value: `${channel.toString()} (${channel.name})`
+                },
+                {
+                    name: "Message",
+                    value: message.content
+                }
+            ],
+            timestamp: new Date(),
+            footer: {
+                text: client.user.tag,
+                icon_url: client.user.avatarURL
+            }
+        }
+        await channels.staff_logs.send({embed});
+    };
+
     events.attach("messageUpdate", editEvent);
+    events.attach("messageDelete", deleteEvent);
 }
