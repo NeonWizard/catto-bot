b192beefe6bc123e5cae3037864cf75fbf1d833d
Spooky <averagewizard13@gmail.com>
2020-01-09 16:50:41 -0700

    Add temp code to populate DB with all users

--- plugins/memberJoin.js
+++ plugins/memberJoin.js
@@ -1,11 +1,20 @@
 const {promisify} = require('util');
 
 module.exports = async function() {
-	const { client, config, dbconn, events } = this;
-	if (config.development) return;
+	const { client, config, dbconn, guild_id, events } = this;
+	// if (config.development) return;
 
 	const _queryAsync = promisify(dbconn.query).bind(dbconn);
 
+	// const readyEvent = async () => {
+	// 	const guild = client.guilds.get(guild_id);
+
+	// 	for (const [id, member] of guild.members.entries()) {
+	// 		await _queryAsync("INSERT IGNORE INTO User SET ?", {id: id, initialJoinDate: member.joinedAt});
+	// 	}
+	// };
+
+	// events.attach("post-ready", readyEvent);
 	events.attach("guildMemberAdd", async (member) => {
 		await _queryAsync("INSERT IGNORE INTO User SET ?", {id: member.id});
 	});
